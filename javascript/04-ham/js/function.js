bed2a9fb47e0e56935d23bfeb4120a4133b44afd
thanhk <thanhkhuatdac@gmail.com>
2018-11-19 22:31:09 +0700

    [javascript]: Bai 3-4: bien va ham trong javascript

---
+++ javascript/04-ham/js/function.js
@@ -0,0 +1,11 @@
+// Hien thi ten va nghe nghiep cua 1 nguoi nao do.
+function showMessage(user, job) {
+	document.write("Xin chao " + user + ". <br/>");
+	document.write("Nghe Nghiep: " + job + ". <br/>");
+}
+
+// hien thi ten cua 1 nguoi.
+function showFullName(firstName, lastName) {
+	var fullName = firstName +" "+ lastName;
+	return fullName;
+}
